8693c421e27745ec2b0b1b42d8f165e0f566525e
jesse <jesse.salviano@outlook.com>
2024-06-02 20:33:51 -0300

    commit inicial

---
+++ components/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div id="div-logo"></div>
+
+    <div id="div-links">
+        <ul class="header-list">
+            <li class="header-item"><a href="#" class="header-link">Funcionalidade</a></li>
+            <li class="header-item"><a href="#" class="header-link">Planos e Preços</a></li>
+            <li class="header-item"><a href="#" class="header-link">Fale conosco</a></li>
+        </ul>
+    </div>
+
+    <div class="div-acesso">
+        <a id="experimente" class="bold" href="#">Experimente Grátis</a>
+        <a href="#" id="entrar" class="bold">Entrar </a>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hermyr - Gerenciador de Notas Fiscais</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+</head>
+<body>
+
+<?php include 'components/header.php' ?>
+    <main>
+        <h1>Comece seu teste grátis!</h1>
+    </main>
+    
+</body>
+</html>
