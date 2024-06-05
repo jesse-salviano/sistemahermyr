22f400ff49140f08ce320639ee7863c4a3623b5e
Lucas Adryell Ramalho <adryelll@gmail.com>
2024-06-05 00:10:54 -0300

    Página Cadastro - Feito

--- components/header.php
+++ components/header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <div class="div-header" id="div-logo">
-        <div id="logo"></div>
+        <a href="index.php"><div id="logo"></div></a>
     </div>
 
     <div class="container text-center" id="div-links">
@@ -18,7 +18,7 @@
     </div>
 
     <div id="div-acesso" class="div-header">
-        <a id="experimente" class="bold" href="RegisterPage.php">Experimente Grátis</a>
+        <a id="experimente" class="bold" href="registerPage.php">Experimente Grátis</a>
         <a href="#" id="entrar" class="bold">Entrar </a>
     </div>
 </header>
